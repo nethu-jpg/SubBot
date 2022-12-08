76e4218d3e09b406c52b4e41abfdbbc01e1c4c20
nethu-jpg <119031346+nethu-jpg@users.noreply.github.com>
2022-12-08 13:58:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2,6 +2,7 @@
 import asyncio
 import re
 import ast
+import random
 from Script import script
 import pyrogram
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, make_inactive
