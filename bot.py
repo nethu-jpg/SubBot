b0c3aae93da2bfa6b56c902cf889817f253320a7
nethu-jpg <119031346+nethu-jpg@users.noreply.github.com>
2022-12-22 15:59:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import logging.config
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.ERROR)
+logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
