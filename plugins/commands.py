fb413fc5cbdb3b4b3e0492dbae29a8caa8e8df3c
nethu-jpg <119031346+nethu-jpg@users.noreply.github.com>
2024-03-20 05:22:03 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -45,7 +45,7 @@ async def start(client, message):
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
-            parse_mode='html'
+            parse_mode='markdown'
         )
         return
     if AUTH_CHANNEL and not await is_subscribed(client, message):
@@ -83,7 +83,7 @@ async def start(client, message):
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
-            parse_mode='html'
+            parse_mode='markdown'
         )
         return
     file_id = message.command[1]
