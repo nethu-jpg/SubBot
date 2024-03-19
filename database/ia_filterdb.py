7193926b857d9df921f5f293112aabbf90444627
nethu-jpg <119031346+nethu-jpg@users.noreply.github.com>
2024-03-20 05:01:52 +0530

    2024

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -36,6 +36,7 @@ async def save_file(media):
 
     # TODO: Find better way to get same file_id for same media to avoid duplicates
     file_id, file_ref = unpack_new_file_id(media.file_id)
+    file_name = re.sub(r"@\w+|(_|\-|\.|\+)", " ", str(media.file_name))
 
     try:
         file = Media(
